186877152ca1e0c480a2182a603623325eac2e50
V1jayy <patidarV1jay@users.noreply.github.com>
2023-11-01 12:40:46 +0530

    feat:T04: app.listen to app.get

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ const isEmail = "some@gmail.com";
 const app = express();
 const port = process.env.PORT || 3000
 
-app.listen(port,() =>{
-    console.log(`listening on port {port}`)
+app.get("/",(req,res)=>{
+    res.send("hello")
 })
