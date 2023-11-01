88fc50059fb1fd8e2dcfb752255d7c5bd6b2108b
V1jayy <patidarV1jay@users.noreply.github.com>
2023-11-01 12:36:51 +0530

    feat:T04: declared app.listen

--- src/app.js
+++ src/app.js
@@ -4,3 +4,8 @@ import express from "express";
 
 const isEmail = "some@gmail.com";
 const app = express();
+const port = process.env.PORT || 3000
+
+app.listen(port,() =>{
+    console.log(`listening on port {port}`)
+})
